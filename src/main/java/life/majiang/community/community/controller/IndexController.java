f48a1adb0ecd05af3aa749fe172cdc871c12df8c
rou-bai <2802607350@qq.com>
2020-11-16 16:09:14 +0800

    增加首页导航条

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class IndexController {
 //    @ResponseBody
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
+//    public String hello(@RequestParam(name = "name") String name, Model model){
+//        model.addAttribute("name", name);
+//        return "index";
+//    }
 }
