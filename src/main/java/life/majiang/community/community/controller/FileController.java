dfc8e27a88acaa62f534bfa74cb4da7b4a1e2ccf
rou-bai <2802607350@qq.com>
2020-12-04 17:35:21 +0800

    支持图片上传

--- src/main/java/life/majiang/community/community/controller/FileController.java
+++ src/main/java/life/majiang/community/community/controller/FileController.java
@@ -4,17 +4,41 @@ import life.majiang.community.community.dto.FileDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Controller
 public class FileController {
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
+    public FileDTO upload(@RequestParam(value = "editormd-image-file", required = false) MultipartFile file) {
         FileDTO fileDTO = new FileDTO();
-        fileDTO.setSuccess(1);
-        fileDTO.setUrl("/images/fuli.jpeg");
-        fileDTO.setMessage("成功");
+        try {
+//            String osName = System.getProperty("os.name");
+            String path = "/Users/tangyao/Downloads/community/src/main/resources/static/upload/";
+            File myfile = new File(path);
+            if (!myfile.exists()) {
+                myfile.mkdirs();
+            }
+
+            //获取上传图片名称
+            String fileName = file.getOriginalFilename();
+            String uuid = UUID.randomUUID().toString().replace("-", "");
+            fileName = uuid + "-" + fileName;
+            file.transferTo(new File(path, fileName));
+
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl("/upload/" + fileName);
+            fileDTO.setMessage("上传成功");
+        } catch (Exception e) {
+            System.out.println(e);
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+        }
         return fileDTO;
     }
 }
